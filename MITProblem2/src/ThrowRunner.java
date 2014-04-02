b2862c0bbbc6c60e576db731557f63f8233d1c7d
Paul Maddock <paul.a.maddock@gmail.com>
2014-04-01 23:15:49 -0400

    assignment 2

---
+++ MITProblem2/src/ThrowRunner.java
@@ -0,0 +1,11 @@
+/**
+ * Created by paulmaddock on 3/30/14.
+ */
+public class ThrowRunner {
+    public static void main(String[] args){
+        //Create form
+        ThrowForm thform = new ThrowForm();
+        thform.setVisible(true);
+    }
+
+}
